e6a2d264814cb21ccf287cd0e54d4dc7dabc6b14
Isaí Arellano <isai.arellano@outlook.com>
2022-06-17 14:48:44 -0500

    Initial Commit

---
+++ Document-Scan-SwiftUI/Document_Scan_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Document_Scan_SwiftUIApp.swift
+//  Document-Scan-SwiftUI
+//
+//  Created by Isaí on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Document_Scan_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
